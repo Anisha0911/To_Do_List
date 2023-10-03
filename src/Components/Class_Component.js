0db269247214022e21335ee681c9b0ef46bf2cf8
Anisha Prajapati <121660716+Anisha0911@users.noreply.github.com>
2023-10-03 12:14:56 +0530

    TO_DO_LIST_ADD

--- src/Components/Class_Component.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-class Class_Comp extends React.Component{
-    render(){
-        return <h1>Hello World</h1>
-    }
-}
-export default Class_Comp;
--- src/Components/Function_Component.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-function Function_Comp(){
-    return <h1>This Data is Coming From Function Component</h1>
-}
-export default Function_Comp;
